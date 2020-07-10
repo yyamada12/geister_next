08056d273dcfd554f3f19a9e85955cd34815848d
yyamada12 <12yacropolisy@gmail.com>
2020-07-10 09:52:54 +0900

    use css modules at Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
+import styles from "../css_modules/home.module.css";
+
 import { useSetPlayer } from "../contexts/playerContext";
 
 const Home: React.FC = () => {
@@ -13,14 +15,14 @@ const Home: React.FC = () => {
   const { setPlayerName } = useSetPlayer();
 
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">Geister</h1>
+      <div className={styles.main}>
+        <h1>Geister</h1>
 
         <TextField
           label="name"
@@ -40,33 +42,7 @@ const Home: React.FC = () => {
             start
           </Button>
         </Link>
-      </main>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      </div>
     </div>
   );
 };
